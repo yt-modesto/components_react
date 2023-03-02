e2340f96aea08c064a81e4188ab786be48d542fb
dirigindo-meu-fusca <clodoaldo.neto.116@ufrn.edu.br>
2023-03-02 11:10:04 -0300

    create first componente Home

---
+++ atoms/src/pages/Home/Home.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Home from ".";
+
+describe("testing for the home component <Home />", () => {
+  test("should render Home correctly", () => {
+    render(<Home />);
+    const home = screen.getByRole("heading", { name: /bem vindo!/i });
+    expect(home).toBeInTheDocument();
+  });
+});
---
+++ atoms/src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>bem vindo!</h1>
+    </div>
+  );
+}
